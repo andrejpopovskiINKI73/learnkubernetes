c9c37ee88c512ae179d625db1aaf7c0c37da6616
andrejpopo <andrejpopovski@hotmail.com>
2021-12-16 15:26:18 +0100

    deployment of the rest of the app (webapp, logic) made changes to frontend app to post request to webapp service IP

--- sa-frontend/src/App.js
+++ sa-frontend/src/App.js
@@ -20,7 +20,7 @@ class App extends Component {
     };
 
     analyzeSentence() {
-        fetch('http://localhost:8080/sentiment', {
+        fetch('http://172.23.47.130:30685/sentiment', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json'
